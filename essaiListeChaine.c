372922c11db4b4e4abf36e0829352f8e58200ced
Diosx17 <bmelon06800@gmail.com>
2022-11-27 13:51:32 +0100

    Arborescence

--- essaiListeChaine.c
+++ essaiListeChaine.c
@@ -2,55 +2,55 @@
 
 typedef struct Noeud
 {
-    int valeur;
-    struct Noeud *pointeur;
+    int valeur; // Un maillon possède une valeur
+    struct Noeud *pointeur; // et un pointeur vers le maillon suivant
 }Noeud;
 
 void printNoeud(Noeud *entree)
 {
-    printf("[%d] -> ",entree->valeur);
+    printf("[%d] -> ",entree->valeur); // on affiche la valeur du maillon pointé
 }
 
 void printListeChaine(Noeud *entree)
 {
-    Noeud *temp = entree;
-    while(temp!=NULL)
+    Noeud *temp = entree; // pour eviter de modifier la liste on en crée une temporaire
+    while(temp!=NULL) // tant que le maillon sur lequel nous sommes n'est pas nul
     {
-        printNoeud(temp);
-        temp = temp->pointeur;
+        printNoeud(temp); // On utilise la fonction plus haut pour afficher
+        temp = temp->pointeur; // on passe au maillon suivant
     }
-    printf("NULL");
+    printf("NULL"); // on affiche la valeur du dernier maillon, donc le maillon nul
     printf("\n");
 }
 
 Noeud *AjouterElement(Noeud *Liste, int valeur)
 {
-    Noeud *nouveau = malloc(sizeof(Noeud));
-    nouveau->valeur = valeur;
-    nouveau->pointeur = Liste;
-    return nouveau;
+    Noeud *nouveau = malloc(sizeof(Noeud)); // Pour ajouter un élément a la liste il faut allouer de l'espace pour donner une adresse au pointeur et éviter ainsi les erreurs potentielles
+    nouveau->valeur = valeur; // on met la valeur en paramètre au maillon
+    nouveau->pointeur = Liste; // on fait pointer le maillon crée vers la liste courrante, autrement dit, on ajoute le maillon a gauche de la chaine
+    return nouveau; // on retourne la totalité de la chaine du coup
 }
 
 Noeud *RemplirAlea(Noeud *Liste,int size)
 {
-    int i = 0;
-    while(i<size)
+    int i = 0; 
+    while(i<size) // On ajouté size Maillon a la liste
     {
-        Liste = AjouterElement(Liste,rand()%100);
+        Liste = AjouterElement(Liste,rand()%100); // on utilise la fonction ajouterElement pour en ajouter un de valeur aléatoire
         i++;
     }
-    return Liste;
+    return Liste; // On retourne toute la liste
 }
 
-Noeud *FreeListeC(Noeud *Liste)
+Noeud *FreeListeC(Noeud *Liste) 
 {
-    Noeud *tmp;
+    Noeud *tmp; // On crée une liste temporaire
     int acc =  0;
     while(Liste)
     {
-        tmp = Liste;
-        Liste = Liste->pointeur; 
-        free(tmp);
+        tmp = Liste; // On donne l'adresse de Liste a tmp
+        Liste = Liste->pointeur; //on parcourt toute la liste 
+        free(tmp); // on libère le maillon courant
        
         acc++;
     }
--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-    srand(time(NULL));
-    Noeud *entree = NULL;
-    entree = RemplirAlea(entree,50);
-    printListeChaine(entree);
-    FreeListeC(entree);
+    srand(time(NULL)); // Pour l'aléatoire 
+    Noeud *entree = NULL; // Initialisation d'une liste chainée
+    entree = RemplirAlea(entree,50);// ajout de 50 maillons à cette liste
+    printListeChaine(entree); // affichage de la liste
+    FreeListeC(entree); // liberation espace memoire reservé dynamiquement
     return 0;
 }
