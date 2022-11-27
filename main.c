978cc969238cf0b079411e9580d079cc179dd711
Diosx17 <bmelon06800@gmail.com>
2022-11-27 13:56:49 +0100

    Update 27/11

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argc, char const *argv[])
 {
     srand(time(NULL)); // Pour l'aléatoire 
     Noeud *entree = NULL; // Initialisation d'une liste chainée
-    entree = RemplirAlea(entree,50);// ajout de 50 maillons à cette liste
+    entree = RemplirAlea(entree,20);// ajout de 50 maillons à cette liste   
     printListeChaine(entree); // affichage de la liste
     FreeListeC(entree); // liberation espace memoire reservé dynamiquement
     return 0;
